e19fdcd94faa0d84db30a7d9883a3be0df3bf017
Ryan Shao <xiaoping.shao@oqton.com>
2023-07-20 09:38:09 +0800

    add the example appbar and appdrawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import ProTip from "./components/ProTip";
 import StickyFooter from "./components/StickyFooter";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { AppGlobalProvider } from "./components/AppGlobalProvider";
+import { HabitListAppBar } from "./components/AppBar";
+import HabitListAppDrawer from "./components/AppDrawer/AppDrawer";
 
 export default function App() {
+  const defaultTheme = createTheme();
   return (
-    <>
-      <Container>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Material UI Create React App example in TypeScript
-          </Typography>
-          <ProTip />
-        </Box>
-      </Container>{" "}
+    <ThemeProvider theme={defaultTheme}>
+      <AppGlobalProvider>
+        <HabitListAppBar />
+        <HabitListAppDrawer />
+        <Container>
+          <Box sx={{ my: 4 }}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Material UI Create React App example in TypeScript
+            </Typography>
+            <ProTip />
+          </Box>
+        </Container>{" "}
+      </AppGlobalProvider>
       <StickyFooter />
-    </>
+    </ThemeProvider>
   );
 }
--- src/components/StickyFooter.tsx
+++ src/components/StickyFooter.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
@@ -19,36 +19,32 @@ function Copyright() {
   );
 }
 
-const defaultTheme = createTheme();
-
 export default function StickyFooter() {
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <CssBaseline />
       <Box
+        component="footer"
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh",
+          py: 3,
+          px: 2,
+          mt: "auto",
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
         }}
       >
-        <CssBaseline />
-        <Box
-          component="footer"
-          sx={{
-            py: 3,
-            px: 2,
-            mt: "auto",
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.grey[200]
-                : theme.palette.grey[800],
-          }}
-        >
-          <Container maxWidth="sm">
-            <Copyright />
-          </Container>
-        </Box>
+        <Container maxWidth="sm">
+          <Copyright />
+        </Container>
       </Box>
-    </ThemeProvider>
+    </Box>
   );
 }
